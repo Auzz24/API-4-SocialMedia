71964907599a6718e79d1d9a37e8364c3d6dc8a5
Austin Miller <austin.miller1424@gmail.com>
2022-03-22 18:50:33 -0500

    user Schema filled with date

--- models/user.js
+++ models/user.js
@@ -1,7 +1,31 @@
 const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
-
+    userName:{
+        type: String, 
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email:{
+        type: String, 
+        required: true,
+        trim: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'This is not a valid emial']
+    },
+    thoughts:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+        }
+    ],
+    firends:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    ]
 })
 
 const User = model('User', UserSchema);
