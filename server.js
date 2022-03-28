bb11ef09962dc611c6b6ac3ff595b9f166198edb
Austin Miller <austin.miller1424@gmail.com>
2022-03-28 16:55:37 -0500

    finally gto thought routes connected to user routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/APISocial', {
+
 });
 
 // Use this to log mongo queries being executed!
